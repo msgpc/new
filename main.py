2422467c38d30c5eb9475e324f01244266908453
pulikesimeme <95121013+pulikesimeme@users.noreply.github.com>
2022-02-06 23:30:59 +0530

    Initialize patch-1

---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+import time
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import requests  
+from bs4 import BeautifulSoup
+
+bot = Client(
+    "Remove FwdTag",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+
+
+START_TXT = """
+Hi {}, I'm Forward Tag Remover bot.\n\nForward me some messages, i will remove forward tag from them.\nAlso can do it in channels.
+"""
+
+START_BTN = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Source Code', url='https://github.com/samadii/ChannelForwardTagRemover'),
+        ]]
+    )
+
+
+@bot.on_message(filters.command(["start"]))
+async def start(bot, update):
+    text = START_TXT.format(update.from_user.mention)
+    reply_markup = START_BTN
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
+
+@bot.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
+async def link_handler(c, m):
+    headers = {
+        "User-agent": 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36'}
+    URL = 'https://9xbuddy.com/process?url=https://twitter.com/dreamforce/status/922920747709820928?lang=fa'
+    page = await requests.get(URL, headers=headers)
+    time.sleep(8)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    time.sleep(8)
+    links = soup.findAll('a')
+    for link in links:
+        link_url = link["href"]
+        await m.reply(f"{link_url}")
+
+
+bot.run()
