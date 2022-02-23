625aba038a4a3d09f681c1eb09a092aef87b553e
pulikesimeme <95121013+pulikesimeme@users.noreply.github.com>
2022-02-23 10:39:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def fwdrmv(c, m):
             caption = caption.replace("HEVC","#HEVC")
             caption = caption + "\n\n**📥 JOIN : @Pulikesi_Meme**"
     try:
-        if m.media:
+        if (m.document | m.video | m.audio ):
             await m.copy(m.chat.id, caption)
             await m.delete()
         else:
