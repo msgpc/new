e8858b88f22cb95cab8a332d3b4bd05374968b32
pulikesimeme <95121013+pulikesimeme@users.noreply.github.com>
2022-05-13 22:50:45 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config(object):
+      BOT_TOKEN = "5133900832:AAFUJpDqYgvEBZJPc_jb01KAaF-0DnyjaZE"
+      API_ID = 1951240342
+      API_HASH = "a858b1feb44196c86e8ef1fb0f86f63a"
